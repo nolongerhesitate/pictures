199574017d5d2fcd2f1c567c1911ee2d0c06f853
nolongerhesitate <nolongerstay@hotmail.com>
2024-09-20 20:18:40 +0800

    feat(login): refactor the login module with chakra-ui

--- app/auth/signin/page.jsx
+++ app/auth/signin/page.jsx
@@ -1,12 +1,32 @@
 import Logo from "@/app/ui/logo";
 import LoginForm from "@/app/ui/login-form";
+import { Flex, Text, Box } from "@chakra-ui/react";
 
 export default function Page() {
   return (
-    <main className="flex flex-col justify-center items-center">
+    <Flex
+      direction="column"
+      justify="center"
+      align="center"
+      height="100vh"
+      width="100vw"
+      gap={6}
+    >
+      {/* <main className="flex flex-col justify-center items-center h-dvh w-dvw"> */}
       <Logo />
+      <Box textAlign="center">
+        <Text fontSize="2xl" as="b"
+          className="my-0 py-0"
+          color="gray.700"
+        >R-Wallpapers</Text>
+        <Text fontSize="xl"
+          className="my-0 py-0"
+          color="gray.600"
+        >Management</Text>
+      </Box>
       <LoginForm />
-    </main>
+    </Flex >
+    // </main>
   );
 }
 
--- app/ui/login-form.jsx
+++ app/ui/login-form.jsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { signIn, signOut } from "next-auth/react"
+import { Input, InputGroup, InputLeftElement, Flex, Button } from "@chakra-ui/react";
+import { EmailIcon, LockIcon } from "@chakra-ui/icons";
+import { signIn } from "next-auth/react";
 import { useState } from "react";
 
 export default function LoginForm() {
-
   const [userInfo, setUserInfo] = useState({ username: "", password: "" });
 
   async function handleSubmit(e) {
@@ -17,34 +18,50 @@ export default function LoginForm() {
     });
 
     // const { error, status, ok, url } = res;
-    console.log(res);
+    console.log("#res:", res);
+    // debugger;
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="flex flex-col bg-black">
-        <input
-          className="bg-black"
-          type="text"
-          id="username"
-          name="username"
-          placeholder="E-mail Address"
-          value={userInfo.username}
-          onChange={({ target }) => { setUserInfo({ ...userInfo, username: target.value }) }}
-          required
-        />
-        <input
-          className="bg-black"
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Your Password"
-          value={userInfo.password}
-          onChange={({ target }) => { setUserInfo({ ...userInfo, password: target.value }) }}
-          required
-        />
-        <button type="submit">Login</button>
-      </div >
+      {/* <div className="h-[400px] flex flex-col justify-between align-center"> */}
+      <Flex
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+        gap={6}
+      >
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <EmailIcon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            placeholder="E-mail Address"
+            type="text"
+            id="username"
+            name="username"
+            value={userInfo.username}
+            onChange={({ target }) => { setUserInfo({ ...userInfo, username: target.value }) }}
+            required
+          />
+        </InputGroup>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <LockIcon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Your Password"
+            value={userInfo.password}
+            onChange={({ target }) => { setUserInfo({ ...userInfo, password: target.value }) }}
+            required
+          />
+        </InputGroup>
+        <Button type="submit">Login</Button>
+        {/* </div> */}
+      </Flex >
     </form >
   );
 }
