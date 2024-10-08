9612cf8eaab389207a9aaa836c5e0b73f866f422
nolongerhesitate <nolongerstay@hotmail.com>
2024-10-08 21:54:46 +0800

    feat: complete the layout of dashboard

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,31 @@
+import { Flex } from "@chakra-ui/react";
+import Header from "../ui/dashboard/header";
+import SideNav from "../ui/dashboard/sidenav";
+
+export default function Layout({ children }) {
+  return (
+    <Flex
+      width="100vw"
+      height="100vh"
+      direction="column"
+      px="10px"
+    >
+      <Header></Header>
+      <Flex
+        width="100vw"
+        height="100vh"
+        mt="10px"
+      >
+        <SideNav
+          mr="10px"
+          p="20px"
+          gap="10px"
+          minW="3xs"
+        ></SideNav>
+        <div className="w-full">
+          {children}
+        </div>
+      </Flex>
+    </Flex>
+  );
+}
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <>
+      <p>This is dashbord</p>
+    </>
+  );
+}
