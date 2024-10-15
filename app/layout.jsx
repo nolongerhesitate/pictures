0507914519de0f413496258c10d86c5f0ff1e2e9
nolongerhesitate <nolongerstay@hotmail.com>
2024-10-15 21:56:39 +0800

    feat(redux): add dependency of redux and provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from './providers';
+import { StrictMode } from "react";
+import StoreProvider from "./store-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,12 +12,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
-    </html>
+    <StrictMode>
+      <html lang="en">
+        <body className={inter.className}>
+          <StoreProvider>
+            <Providers>
+              {children}
+            </Providers>
+          </StoreProvider>
+        </body>
+      </html>
+    </StrictMode>
   );
 }
