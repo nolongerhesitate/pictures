014cf05ad53c2861b26c42f0305466e58059f255
nolongerhesitate <nolongerstay@hotmail.com>
2025-01-22 19:18:50 +0800

    feat: use Toast

--- app/ui/wallpapers/main-content.jsx
+++ app/ui/wallpapers/main-content.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Wrap, Portal } from "@chakra-ui/react";
+import { Wrap, Portal, useToast } from "@chakra-ui/react";
 import GlobalSpinner from "@/app/ui/components/global-spinner";
 import apiUtil from "@/app/lib/utils/apiUtil";
 import { useEffect, useState } from "react";
@@ -9,17 +9,28 @@ import PictureTile from "@/app/ui/wallpapers/picture-tile";
 export default function MainContent({ currentPage, setTotalPages }) {
   const [pictures, setPictures] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
 
   useEffect(() => {
     const fecthData = async () => {
-      setIsLoading(true)
-      const result = await apiUtil.getAllPictures(currentPage);
-      setPictures(result.data);
-      if (result.pagination) {
-        const totalPages = Math.ceil(result.pagination.totalCount / result.pagination.limit);
-        setTotalPages(totalPages);
+      try {
+        setIsLoading(true)
+        const result = await apiUtil.getAllPictures(currentPage);
+        setPictures(result.data);
+        if (result.pagination) {
+          const totalPages = Math.ceil(result.pagination.totalCount / result.pagination.limit);
+          setTotalPages(totalPages);
+        }
+      } catch (error) {
+        toast({
+          title: "error toast",
+          description: "Failed to get pictures!",
+          status: "error",
+          isClosable: true,
+        });
+      } finally {
+        setIsLoading(false);
       }
-      setIsLoading(false);
     };
 
     fecthData();
