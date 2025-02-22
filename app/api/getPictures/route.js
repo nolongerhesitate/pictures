604eede7d72450697f8b06f7bd3081ba36925882
nolongerhesitate <nolongerstay@hotmail.com>
2025-02-22 11:36:18 +0800

    fix: getPicutres bug

--- app/api/getPictures/route.js
+++ app/api/getPictures/route.js
@@ -17,12 +17,25 @@ export async function GET(request) {
     const delete_param = (deleted === null ? undefined : deleted);
     const offset = (page - 1) * limit;
 
-    let sql = `select t1.picture_id from picture_users_relationship as t1 left join pictures as t2 on t1.picture_id = t2.id where t2.display_name like $1`;
+    let sql = `select t2.* from picture_users_relationship as t1 left join pictures as t2 on t1.picture_id = t2.id where t2.display_name like $1`;
     delete_param !== undefined && (sql += ` and t2.is_deleted = $2`);
-    sql += ` and user_id = $3 LIMIT $4 OFFSET $5`;
-    const pictureIds = await querySql(sql, [`%${feed}%`, delete_param, session.user.id, limit, offset]);
+    sql += ` and t1.user_id = $3 order by t2.display_name asc LIMIT $4 OFFSET $5`;
+    let pictures = await querySql(sql, [`%${feed}%`, delete_param, session.user.id, limit, offset]);
 
     // TODO: Optimize
+    pictures.forEach(async item => {
+      sql = `select thumbnail_path, width, height from thumbnails where picture_id = $1`;
+      const thumb = (await querySql(sql, [item.id]))[0];
+      if (thumb) {
+        item.thumbFileName = path.basename(thumb.thumbnail_path);
+        item.thumbWidth = thumb.width;
+        item.thumbHeight = thumb.height;
+      };
+      return item;
+    });
+
+    // console.log("#pictures:", pictures);
+    /*
     let pictures = await Promise.all(pictureIds.map(async item => {
       let sql = `select * from pictures where id = $1`;
       delete_param !== undefined && (sql += ` and is_deleted = $2`);
@@ -43,6 +56,7 @@ export async function GET(request) {
     }));
 
     pictures = pictures.filter(item => item);
+    */
 
     sql = `select count(*) from picture_users_relationship as t1 left join pictures as t2 on t1.picture_id = t2.id where t1.user_id = $1`;
     delete_param !== undefined && (sql += ` and t2.is_deleted = $2`);
